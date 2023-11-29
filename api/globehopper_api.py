901090bb16c68914723045cbead36ed514041e6d
ARNAB DAS <raj713335@gmail.com>
2023-11-29 14:31:12 +0530

    :national_park: Added image api to fetch images from pexels

--- api/globehopper_api.py
+++ api/globehopper_api.py
@@ -14,6 +14,9 @@ load_dotenv()
 WEATHER_RAPID_API_HOST = os.getenv("WEATHER_RAPID_API_HOST")
 WEATHER_RAPID_API_KEY = os.getenv("WEATHER_RAPID_API_KEY")
 
+PEXELS_API_HOST = os.getenv("PEXELS_API_HOST")
+PEXELS_API_KEY = os.getenv("PEXELS_API_KEY")
+
 globehopper_Blueprint = Blueprint('globehopper_Blueprint', __name__)
 
 
@@ -38,6 +41,23 @@ def get_weather_data():
             "X-RapidAPI-Host": WEATHER_RAPID_API_HOST
         }
         response = requests.get(url, headers=headers, params=querystring)
+        return jsonify(response.json()), status.HTTP_200_OK
+    except Exception as err:
+        return jsonify({"message": f"Module - Error - {err}"}), status.HTTP_400_BAD_REQUEST
+
+
+@globehopper_Blueprint.route('/images', methods=['POST'])
+def get_images():
+    input_payload = request.get_json(cache=False)
+    logging.info("Request to fetch location Data - %s", input_payload['parameters']['location'])
+    location = str(input_payload['parameters']['location'])
+    query_count = str(input_payload['parameters']['query_count'])
+    try:
+        url = "https://" + PEXELS_API_HOST + "/v1/search?query=" + location + "&per_page=" + query_count
+        headers = {
+            "Authorization": PEXELS_API_KEY
+        }
+        response = requests.get(url, headers=headers)
         return jsonify(response.json()), status.HTTP_200_OK
     except Exception as err:
         return jsonify({"message": f"Module - Error - {err}"}), status.HTTP_400_BAD_REQUEST
