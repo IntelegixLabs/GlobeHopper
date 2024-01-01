c1414d3e12c90448c13e66c4b66d64e024f01dcc
ARNAB DAS <raj713335@gmail.com>
2024-01-01 12:22:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ rth = logging.handlers.RotatingFileHandler(
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s  : %(message)s', handlers=[rth])
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
